6340cd0e65c246adeda8fb1aed78a438dd6ceb25
csce313tyagi <111099890+csce313tyagi@users.noreply.github.com>
2022-08-12 09:14:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# <p align="center">LAB1A: Basic Client/Server Communication<p>
+
+**Introduction**
+
+The objective of this lab exercise is to introduce concepts of PA1 and prepare you for the first mainstream programming assingment. To be done in lab with guidance from a TA.
---
+++ server.cpp
@@ -0,0 +1,17 @@
+#include "common.h"
+
+using namespace std;
+
+// typedef vector<string> string_vector;
+// typedef vector<string_vector> string_vector2D;
+
+// string_vector who {"The wolf", "The music", "An eagle", "The rain"};
+// string_vector what {"played", "sits", "sleeps", "crashes"};
+// string_vector how {"loudly", "quietly", "quickly", "slowly", "softly"};
+// string_vector where {"through the forest.", "in the sky.", "at night.", "in the tree.", "at dinner."};
+
+// string_vector2D mat {who, what, how, where};
+
+int main () {
+    
+}
